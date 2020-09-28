3c132a6931093a133382187d39a2d620eadfb3d1
Seth Blanchard <nsethblanchard@gmail.com>
2020-09-28 12:37:19 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,16 @@
 # Use this space to code along with the readme
 
+def greeting 
+    puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+
+
 phrase = "Hello World!"
 puts phrase
 puts phrase
